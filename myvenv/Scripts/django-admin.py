cd51ab4527b35852cf02d8bf6deefb687fe5c749
Your Name <you@example.com>
2017-07-31 15:18:01 -0400

    ok

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Admin\Desktop\src\AVPgwc2017.github.io\djangogirls\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
